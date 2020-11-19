2087cdc62921db359914da9441341d3af6929c45
braynMjn <brainmjn11@gmail.com>
2020-11-20 01:02:41 +0545

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is a Todo List App. It is an app created while I was learning vueJS crash course in Youtube from Traversy Media.</p>
+  </div>
+</template>
+
+<style scoped>
+p{
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 24px;
+}
+</style>
